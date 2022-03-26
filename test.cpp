f2410f32fb411190e9592525e4dc37da7ab25118
Luis <tonyyluis@hotmail.com>
2022-03-26 15:31:43 -0500

    parse a test CSV, unique item array

---
+++ test.cpp
@@ -0,0 +1,75 @@
+#include <fstream>
+#include <iostream>
+#include <string>
+#include <bits/stdc++.h>
+
+using namespace std;
+
+string convertToString(char* a)
+{
+    string s = a;
+    return s;
+}
+
+int main()
+{
+    string sources[100];
+    int scount = 0;
+    bool dup = true;
+    char line[15];
+    string sline;
+    ifstream inputFile;
+    inputFile.open("testCSV2.csv");
+
+    cout << "Hello World!\n";
+    
+    int count = 1;
+
+    while(inputFile)
+    {
+
+        inputFile.getline(line, 256, ',');
+        if (count == 3)
+        {
+            sline = convertToString(line);
+            dup = true;
+            for (int i = 0; i <= scount; i++)
+            {
+                if (scount == 0)
+                {
+                    dup = true;
+                    break;
+                }
+                if (sources[i].compare(sline) == 0)
+                {
+                    dup = false;
+                    break;
+                }
+            }
+
+             if (dup)
+                cout<< 1;
+            else
+                cout << 0;
+
+            cout <<"cv:"<<line <<"sv:"<< sline << "\n";
+           
+            if(dup && scount < 100)
+            {
+                sources[scount] = sline;
+                scount++;
+            }
+        }
+        count++;
+
+        if (count > 11)
+        {
+            count = 2;
+        }
+    }
+
+    for (int i = 0; i<= scount; i++)
+    {
+        cout << sources[i]<<"\n";
+    }
+}
